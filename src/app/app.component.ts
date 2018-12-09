8659a09fdf800296f3ec4f2c9f6f487ce92bb298
olee <leeouhyoun@gmail.com>
2018-12-09 15:59:15 +0900

    setting & test sources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'naak-studio';
+    constructor() {
+        this.getTitle();
+    }
+    title = 'naak-studio';
+    public getTitle() {
+        $.each(Array.of(' H', 'e', 'l', 'l', 'o'), (_index, item) => {
+            this.title = this.title + item;
+        });
+    }
 }
