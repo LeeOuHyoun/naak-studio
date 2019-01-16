3874997652437a2e25c0de115e86eb70fe1cd027
DESKTOP-3R3N12F\o-lee <o-lee@fores.jp>
2019-01-16 17:46:23 +0900

    勉強中...

--- src/app/contents/main/app.contents.main.ts
+++ src/app/contents/main/app.contents.main.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-// import * as $ from 'jquery';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     templateUrl: './app.contents.main.html',
     styleUrls: ['./app.contents.main.scss']
 })
+
 export class AppContentsMainComponent {
-    constructor() { }
+    constructor(private http: HttpClient) {
+        console.log('Hello AppContentsMainComponent ~ !!');
+        this.getPosts();
+    }
+    getPosts() {
+        this.http.get('../assets/json/db.json').subscribe((data) => {
+            console.log(JSON.stringify(data));
+        }, err => {
+            console.log('error___');
+        });
+    }
+
+    getPosts2() {
+        // https://jsonplaceholder.typicode.com/posts
+        this.http.get('../assets/json/dummyDB.json')
+        .toPromise()
+        .then((data) => {
+            console.log('hello~~');
+            console.log(JSON.stringify(data));
+        }).catch((error) => {
+            console.log('error ===>');
+        });
+    }
 }
