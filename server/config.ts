8b711d722177c130c899bed260d878f272a9b937
olee <leeouhyoun@gmail.com>
2019-03-15 23:57:21 +0900

    中。。。。いろいろ

--- server/config.ts
+++ server/config.ts
@@ -1,8 +1,8 @@
 export const serverPort = 3000;
 export const dbAccessOptions = {
     host: 'localhost',
-    port: 5440,
-    database: 'medalstation',
-    user: 'rdb_admin',
-    password: 'PassW0rd'
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: 'postgresql'
 };
